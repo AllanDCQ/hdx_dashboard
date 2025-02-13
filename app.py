cd569d8c28f0b3cfaf111e5b2608f553381167bf
Allan <allan.dc@hotmail.fr>
2025-02-13 08:20:34 +0100

    Deploy render

--- app.py
+++ app.py
@@ -448,4 +448,4 @@ def display_status_page():
 port = int(os.environ.get("PORT", 8080))
 # Run the Dash app
 if __name__ == "__main__":
-    app.run_server( debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
